3067bbb3c82069e50b33ebee85f04014f619f528
Rolando Medina <rolando.ms@outlook.com>
2020-05-02 20:58:24 -0500

    Replaced localhost url binding to 0.0.0.0 for deploy

--- Repos.Web.Admin/Startup.cs
+++ Repos.Web.Admin/Startup.cs
@@ -55,7 +55,7 @@ namespace Repos.Web.Admin
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
